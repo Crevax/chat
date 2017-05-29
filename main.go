c9e4bd1de340774f20c8e6881c54a5521d129c12
CJ Davis <cj@cjdavis.me>
2017-05-29 15:27:02 -0400

    Add OAuth via google

--- main.go
+++ main.go
@@ -9,12 +9,16 @@ import (
 
 	"github.com/crevax/chat/templating"
 	"github.com/crevax/chat/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 func main() {
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templating.TemplateHandler{Filepath: []string{"templates", "html", "chat.html"}})
+	http.Handle("/chat", MustAuth(&templating.TemplateHandler{Filepath: []string{"templates", "html", "chat.html"}}))
+	http.Handle("/login", &templating.TemplateHandler{Filepath: []string{"templates", "html", "login.html"}})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	go r.run()
@@ -23,6 +27,12 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
+
+	gomniauth.SetSecurityKey(os.Getenv("OATH_SECRET_KEY"))
+	gomniauth.WithProviders(
+		google.New(os.Getenv("GOOGLE_OAUTH_ID"), os.Getenv("GOOGLE_OAUTH_SECRET"),
+			"http://localhost:8080/auth/callback/google"),
+	)
 
 	log.Println("Starting web server on", port)
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
