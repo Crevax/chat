fbba2c47c029e5880dd592c606e964d48c11b356
CJ Davis <cj@cjdavis.me>
2017-05-29 15:48:03 -0400

    Fix typo in trace

--- room.go
+++ room.go
@@ -44,7 +44,7 @@ func (r *room) run() {
 			close(client.send)
 			r.tracer.Trace("Client left")
 		case msg := <-r.forward:
-			r.tracer.Trace("Message receive: ", string(msg))
+			r.tracer.Trace("Message received: ", msg.Message)
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
